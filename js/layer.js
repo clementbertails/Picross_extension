eaf0ba58fc41146e439a0434923462c8a3d74f9a
Clément Bertails <clement.bertails@gmail.com>
2024-03-26 12:13:01 +0100

    refactor code and patching style

--- js/layer.js
+++ js/layer.js
@@ -1,11 +1,6 @@
-function createLayer() {
-
+function getKey_top() {
     let key_top = document.getElementsByClassName('key top');
-    let key_left = document.getElementsByClassName('key left');
-
     let key_top_values = [];
-    let key_left_values = [];
-
     for (let i = 0; i < key_top.length; i++) {
         let sum = 0;
         let len = 0;
@@ -15,7 +10,12 @@ function createLayer() {
         });
         key_top_values.push(sum + len - 1);
     }
+    return key_top, key_top_values;
+}
 
+function getKey_left() {
+    let key_left = document.getElementsByClassName('key left');
+    let key_left_values = [];
     for (let i = 0; i < key_left.length; i++) {
         let sum = 0;
         let len = 0;
@@ -25,21 +25,13 @@ function createLayer() {
         });
         key_left_values.push(sum + len - 1);
     }
+    return key_left, key_left_values;
+}
+
+function displayRightLayer(key_left_values) {
 
     let grid = document.getElementById('puzzle').querySelector('tbody');
     let grid_nbrows = parseInt(localStorage.getItem('picross.dimensionHeight').replace('"', ''));
-    let grid_nbcols = parseInt(localStorage.getItem('picross.dimensionWidth').replace('"', ''));
-
-    while(grid.rows.length > grid_nbrows + 1) {
-        grid.removeChild(grid.lastChild);
-    }
-
-    while(grid.rows[1].childElementCount > grid_nbcols + 1) {
-        for (let i = 0; i < grid_nbrows; i++) {
-            let row = grid.rows[i + 1]
-            row.removeChild(row.lastChild);
-        }
-    }
 
     for (let i = 0; i < grid_nbrows; i++) {
         let row = grid.rows[i + 1];
@@ -47,23 +39,79 @@ function createLayer() {
         let cell = document.createElement('td');
         cell.className = 'key';
         cell.innerHTML = key_left_values[i];
-        cell.style = "text-align: left";
-        cell.style = "vertical-align: middle";
+        cell.style = "vertical-align: middle; padding-top: 2px; padding-bottom: 2px; padding-left: 2px";
         row.appendChild(cell);
     }
+}
+
+function displayBottomLayer(key_top_values) {
+
+    let grid = document.getElementById('puzzle').querySelector('tbody');
+    let grid_nbcols = parseInt(localStorage.getItem('picross.dimensionWidth').replace('"', ''));
 
     let new_row = grid.appendChild(document.createElement('tr'));
     new_row.appendChild(document.createElement('td')).className = 'key';
     for(var i = 0; i < grid_nbcols; i++) {
-        cell = new_row.appendChild(document.createElement('td'));
+        let cell = document.createElement('td');
         cell.className = 'key';
         cell.innerHTML = key_top_values[i];
-        cell.style = "text-align: center";
-        cell.style = 'paddingTop = "5px"';
+        cell.style = "text-align: center; padding-left: 2px; padding-right: 2px; padding-top: 2px";
+        new_row.appendChild(cell);
     }
 }
 
-document.onload = createLayer();
+function clearLayers() {
+    let grid = document.getElementById('puzzle').querySelector('tbody');
+    let grid_nbrows = parseInt(localStorage.getItem('picross.dimensionHeight').replace('"', ''));
+    let grid_nbcols = parseInt(localStorage.getItem('picross.dimensionWidth').replace('"', ''));
+
+    // right layer clear
+    while(grid.rows.length > grid_nbrows + 1) {
+        grid.removeChild(grid.lastChild);
+    }
+
+    // bottom layer clear
+    while(grid.rows[1].childElementCount > grid_nbcols + 1) {
+        for (let i = 0; i < grid_nbrows; i++) {
+            let row = grid.rows[i + 1]
+            row.removeChild(row.lastChild);
+        }
+    }
+
+
+}
+
+function layers_help_col(key, col) {
+
+}
+
+function layers_help_row(key, row) {
+
+}
+
+function createLayers() {
+
+    let key_top, key_top_values = getKey_top();
+    let key_left, key_left_values = getKey_left();
+
+    clearLayers();
+    displayBottomLayer(key_top_values);
+    displayRightLayer(key_left_values);
+
+        // const isSuperior = (nb) => parseInt(nb) > (grid_nbrows - ky_top_values[i]);
+
+        // actual_key_top = key_top[i].innerHTML.replace(/<\/?em>/g, "").split("<br>");
+        // if (actual_key_top.some(isSuperior)) {
+        //     layers_help_col(actual_key_top, i);
+        // }
+        
+}
+
+function layer_solution() {
+
+}
+
+document.onload = createLayers();
 
 const targets = [
     "click #new",
@@ -85,7 +133,19 @@ targets.forEach(target => {
     const [event, subTarget] = target.split(" ");
     const elements = subTarget ? defaultTarget.querySelectorAll(subTarget) : [ defaultTarget ];
 
-    elements.forEach(element => element.addEventListener(event, createLayer))
+    elements.forEach(element => {
+        if (element == "#new" || element == "#customGame") {
+            element.addEventListener(event, () => {
+                console.log('event ' + event + ' on ' + element);
+                createLayers();
+            });
+        } else {
+            element.addEventListener(event, () => {
+                console.log('event ' + event + ' on ' + element);
+                createLayers();
+            });
+        }
+    });
 });
 
 const containerElements = document.getElementsByClassName('container');
